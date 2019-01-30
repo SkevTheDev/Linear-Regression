32e827a34af51ebaa59f76e93ba4a499dc0c811c
Skev <ataraskewich@gmail.com>
2019-01-29 21:33:13 -0500

    updated lr_1d.py

---
+++ lr_1d.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+#load data
+X = []
+Y = []
+for line in open('data_1d.csv'):
+	x, y = line.split(',')
+	X.append(float(x))
+	Y.append(float(y))
+
+#convert X and Y into numpy arrays
+X = np.array(X)
+Y = np.array(Y)
+
+#plot data
+plt.scatter(X,Y)
+plt.show()
+
+#apply loss function
+denominator = X.dot(X) - X.mean() * X.sum()
+a = (X.dot(Y) - Y.mean() * X.sum()) / denominator
+b = (Y.mean() * X.dot(X) - X.mean() * X.dot(Y) )  / denominator
+
+#compute y-hat (predicted y)
+Yhat = a*X + b
+
+#plot predicted line
+plt.scatter(X,Y)
+plt.plot(X, Yhat)
+plt.show()
