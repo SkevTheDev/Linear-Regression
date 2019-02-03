535c9e5de2d91eee606791eec1f5f5796d18ccb4
Skev <ataraskewich@gmail.com>
2019-02-02 22:48:25 -0500

    created systolic.py

---
+++ systolic.py
@@ -0,0 +1,15 @@
+# https://deeplearningcourses.com/c/data-science-linear-regression-in-python
+# need to sudo pip install xlrd to use pd.read_excel
+# data is from:
+# http://college.cengage.com/mathematics/brase/understandable_statistics/7e/students/datasets/mlr/frames/mlr02.html
+
+# The data (X1, X2, X3) are for each patient.
+# X1 = systolic blood pressure
+# X2 = age in years
+# X3 = weight in pounds
+
+
+
+
+
+
